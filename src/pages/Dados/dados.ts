c0d938859973ae324c1d5428c09424265c68f3c8
Eduardo Müller <eduuardomuller@hotmail.com>
2016-12-04 19:27:09 -0300

    menu

---
+++ src/pages/Dados/dados.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'dados',
+  templateUrl: 'dados.html'
+})
+export class Dados {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
